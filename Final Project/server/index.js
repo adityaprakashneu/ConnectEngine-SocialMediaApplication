d7548a5d13091000309dc310ff4d8b12e8897fc6
Ronak Mahidharia <mahidharia.r@northeastern.edu>
2023-11-30 13:32:13 -0500

    Routes for server backend

--- Final Project/server/index.js
+++ Final Project/server/index.js
@@ -40,3 +40,7 @@ const upload = multer({ storage });
 
 // ROUTES WITH FILES
 app.post("/auth/register", upload.single("picture"), register);
+
+// ROUTES
+app.use("/auth", authRoutes);
+app.use("/users", userRoutes);
