3e255176736c6ad0921b8eb1fdb25e90c9b0ffa6
Ronak Mahidharia <mahidharia.r@northeastern.edu>
2023-11-30 14:31:24 -0500

    Mongoose for server backend

--- Final Project/server/index.js
+++ Final Project/server/index.js
@@ -44,3 +44,12 @@ app.post("/auth/register", upload.single("picture"), register);
 // ROUTES
 app.use("/auth", authRoutes);
 app.use("/users", userRoutes);
+
+// MONGOOSE SETUP
+const PORT = process.env.PORT || 6001;
+mongoose.connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+  useUnifiedTopology: true,
+}).then(() => {
+    app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
+}).catch((error) => console.log(`${error} did not connect`))
