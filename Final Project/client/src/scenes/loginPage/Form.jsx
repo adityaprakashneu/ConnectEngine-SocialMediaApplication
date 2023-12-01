f68a11499c6f621f10102c70244cc9baa5eb8639
mridulapbk08 <prabhakar.m@northeastern.edu>
2023-12-01 11:33:26 -0500

    Initial Value Register

--- Final Project/client/src/scenes/loginPage/Form.jsx
+++ Final Project/client/src/scenes/loginPage/Form.jsx
@@ -26,4 +26,19 @@ const registerSchema = yup.object().shape({
   picture: yup.string().required("required"),
 });
 
+const loginSchema = yup.object().shape({
+  email: yup.string().email("invalid email").required("required"),
+  password: yup.string().required("required"),
+});
+
+const initialValuesRegister = {
+  firstName: "",
+  lastName: "",
+  email: "",
+  password: "",
+  location: "",
+  occupation: "",
+  picture: "",
+};
+
 export default Form;
