520b4bc60c33d5e78b91d85c3912069eb337c375
mridulapbk08 <prabhakar.m@northeastern.edu>
2023-12-02 13:06:14 -0500

    ffff

--- Final Project/client/src/scenes/loginPage/Form.jsx
+++ Final Project/client/src/scenes/loginPage/Form.jsx
@@ -73,5 +73,27 @@ const Form = () => {
     const savedUser = await savedUserResponse.json();
     onSubmitProps.resetForm();    
 
+    if (savedUser) {
+      setPageType("login");
+    }
+  };
 
+  const login = async (values, onSubmitProps) => {
+    const loggedInResponse = await fetch("http://localhost:3001/auth/login", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(values),
+    });
+    const loggedIn = await loggedInResponse.json();
+    onSubmitProps.resetForm();
+    if (loggedIn) {
+      dispatch(
+        setLogin({
+          user: loggedIn.user,
+          token: loggedIn.token,
+        })
+      );
+      navigate("/home");
+    }
+  };
 export default Form;
