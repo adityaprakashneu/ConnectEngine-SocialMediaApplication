c87a41a1afd1c8c3b8fba65ab0391af97fe069bd
Aditya Prakash <prakash.adi@northeastern.edu>
2023-12-05 07:29:28 -0500

    calendar.jsx updated

--- Final Project/client/src/scenes/calendar/calendar.jsx
+++ Final Project/client/src/scenes/calendar/calendar.jsx
@@ -14,3 +14,24 @@ import {
 } from "@mui/material";
 import Header from "../../components/Header";
 import { tokens } from "../../theme";
+
+const Calendar = () => {
+    const theme = useTheme();
+    const colors = tokens(theme.palette.mode);
+    const [currentEvents, setCurrentEvents] = useState([]);
+  
+    const handleDateClick = (selected) => {
+      const title = prompt("Please enter a new title for your event");
+      const calendarApi = selected.view.calendar;
+      calendarApi.unselect();
+  
+      if (title) {
+        calendarApi.addEvent({
+          id: `${selected.dateStr}-${title}`,
+          title,
+          start: selected.startStr,
+          end: selected.endStr,
+          allDay: selected.allDay,
+        });
+      }
+    };
