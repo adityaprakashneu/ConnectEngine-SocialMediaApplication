41f96f973295b091cff8c36e1876caa6abe12aaa
Aditya Prakash <prakash.adi@northeastern.edu>
2023-12-07 06:17:35 -0500

    side bar completed

--- Final Project/client/src/scenes/global/Sidebar.jsx
+++ Final Project/client/src/scenes/global/Sidebar.jsx
@@ -74,3 +74,175 @@ const Sidebar = () => {
     } = user;
   
     console.log(picturePath)
+
+    return (
+        <Box
+          sx={{
+            "& .pro-sidebar-inner": {
+              background: `${colors.primary[400]} !important`,
+            },
+            "& .pro-icon-wrapper": {
+              backgroundColor: "transparent !important",
+            },
+            "& .pro-inner-item": {
+              padding: "5px 35px 5px 20px !important",
+            },
+            "& .pro-inner-item:hover": {
+              color: "#868dfb !important",
+            },
+            "& .pro-menu-item.active": {
+              color: "#6870fa !important",
+            },
+          }}
+        >
+          <ProSidebar collapsed={isCollapsed}>
+            <Menu iconShape="square">
+              {/* LOGO AND MENU ICON */}
+              <MenuItem
+                onClick={() => setIsCollapsed(!isCollapsed)}
+                icon={isCollapsed ? <MenuOutlinedIcon /> : undefined}
+                style={{
+                  margin: "10px 0 20px 0",
+                  color: colors.grey[100],
+                }}
+              >
+                {!isCollapsed && (
+                  <Box
+                    display="flex"
+                    justifyContent="space-between"
+                    alignItems="center"
+                    ml="15px"
+                  >
+                    <Typography variant="h3" color={colors.grey[100]}>
+                      ADMINS
+                    </Typography>
+                    <IconButton onClick={() => setIsCollapsed(!isCollapsed)}>
+                      <MenuOutlinedIcon />
+                    </IconButton>
+                  </Box>
+                )}
+              </MenuItem>
+    
+              {!isCollapsed && (
+                <Box mb="25px">
+                  <Box display="flex" justifyContent="center" alignItems="center">
+                    <img
+                      alt="profile-user"
+                      width="100px"
+                      height="100px"
+                      src={`http://localhost:3001/assets/${picturePath}`}
+                      style={{ cursor: "pointer", borderRadius: "50%" }}
+                    />
+                  </Box>
+                  <Box textAlign="center">
+                    <Typography
+                      variant="h2"
+                      color={colors.grey[100]}
+                      fontWeight="bold"
+                      sx={{ m: "10px 0 0 0" }}
+                    >
+                      {firstName} {lastName}
+                    </Typography>
+                    <Typography variant="h5" color={colors.greenAccent[500]}>
+                      Administrator
+                    </Typography>
+                  </Box>
+                </Box>
+              )}
+    
+              <Box paddingLeft={isCollapsed ? undefined : "10%"}>
+                <Item
+                  title="Dashboard"
+                  to="/admin"
+                  icon={<HomeOutlinedIcon />}
+                  selected={selected}
+                  setSelected={setSelected}
+                />
+    
+                <Typography
+                  variant="h6"
+                  color={colors.grey[300]}
+                  sx={{ m: "15px 0 5px 20px" }}
+                >
+                  Data
+                </Typography>
+                <Item
+                  title="Manage Team"
+                  to="/admin/team"
+                  icon={<PeopleOutlinedIcon />}
+                  selected={selected}
+                  setSelected={setSelected}
+                />
+                <Item
+                  title="Contacts Information"
+                  to="/admin/contacts"
+                  icon={<ContactsOutlinedIcon />}
+                  selected={selected}
+                  setSelected={setSelected}
+                />
+                <Item
+                  title="Invoices Balances"
+                  to="/admin/invoices"
+                  icon={<ReceiptOutlinedIcon />}
+                  selected={selected}
+                  setSelected={setSelected}
+                />
+    
+                <Typography
+                  variant="h6"
+                  color={colors.grey[300]}
+                  sx={{ m: "15px 0 5px 20px" }}
+                >
+                  Pages
+                </Typography>
+                <Item
+                  title="Profile Form"
+                  to="/admin/form"
+                  icon={<PersonOutlinedIcon />}
+                  selected={selected}
+                  setSelected={setSelected}
+                />
+                <Item
+                  title="FAQ Page"
+                  to="/admin/faq"
+                  icon={<HelpOutlineOutlinedIcon />}
+                  selected={selected}
+                  setSelected={setSelected}
+                />
+    
+                <Typography
+                  variant="h6"
+                  color={colors.grey[300]}
+                  sx={{ m: "15px 0 5px 20px" }}
+                >
+                  Charts
+                </Typography>
+                <Item
+                  title="Geography Chart"
+                  to="/admin/geography"
+                  icon={<MapOutlinedIcon />}
+                  selected={selected}
+                  setSelected={setSelected}
+                />
+                <Item
+                  title="Support"
+                  to="/admin/help"
+                  icon={<CalendarTodayOutlinedIcon />}
+                  selected={selected}
+                  setSelected={setSelected}
+                />
+                <Item
+                  title="Terms and Conditions"
+                  to="/admin/help"
+                  icon={<CalendarTodayOutlinedIcon />}
+                  selected={selected}
+                  setSelected={setSelected}
+                />
+              </Box>
+            </Menu>
+          </ProSidebar>
+        </Box>
+      );
+    };
+    
+    export default Sidebar;
