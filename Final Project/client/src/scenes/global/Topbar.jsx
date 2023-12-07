7bc7cecaa91d83ecef4227559a1bda88c35c4531
Aditya Prakash <prakash.adi@northeastern.edu>
2023-12-07 16:38:24 -0500

    top bar functionality added

---
+++ Final Project/client/src/scenes/global/Topbar.jsx
@@ -0,0 +1,20 @@
+import { Box, IconButton, useTheme, FormControl, MenuItem, Select, Typography } from "@mui/material";
+import { useContext } from "react";
+import { ColorModeContext, tokens } from "../../theme";
+import InputBase from "@mui/material/InputBase";
+import LightModeOutlinedIcon from "@mui/icons-material/LightModeOutlined";
+import DarkModeOutlinedIcon from "@mui/icons-material/DarkModeOutlined";
+import NotificationsOutlinedIcon from "@mui/icons-material/NotificationsOutlined";
+import SettingsOutlinedIcon from "@mui/icons-material/SettingsOutlined";
+import PersonOutlinedIcon from "@mui/icons-material/PersonOutlined";
+import SearchIcon from "@mui/icons-material/Search";
+import { useDispatch, useSelector } from "react-redux";
+import { setMode, setLogout } from "state";
+
+const Topbar = () => {
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+  const colorMode = useContext(ColorModeContext);
+  const dispatch = useDispatch();
+
+  const neutralLight = theme.palette.neutral.light;
