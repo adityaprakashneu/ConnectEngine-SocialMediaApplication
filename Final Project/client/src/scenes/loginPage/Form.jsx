540dba4cec4a02da0f99048f9718f3197a2cc693
mridulapbk08 <prabhakar.m@northeastern.edu>
2023-12-08 18:22:46 -0500

    form.jsx

--- Final Project/client/src/scenes/loginPage/Form.jsx
+++ Final Project/client/src/scenes/loginPage/Form.jsx
@@ -53,4 +53,27 @@ const Form = () => {
   const navigate = useNavigate();
   const isNonMobile = useMediaQuery("(min-width:600px)");
   const isLogin = pageType === "login";
-  const isRegister = pageType === "register";
+
+  
+  const register = async (values, onSubmitProps) => {
+    // this allows us to send form info with image
+    const formData = new FormData();
+    for (let value in values) {
+      formData.append(value, values[value]);
+    }
+    formData.append("picturePath", values.picture.name);
+
+    const savedUserResponse = await fetch(
+      "http://localhost:3001/auth/register",
+      {
+        method: "POST",
+        body: formData,
+      }
+    );
+    const savedUser = await savedUserResponse.json();
+    onSubmitProps.resetForm();
+
+    if (savedUser) {
+      setPageType("login");
+    }
+  };
