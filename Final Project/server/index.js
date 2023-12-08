61f7fd02baabb6eff9513f394f74a5f4da4daecb
Ronak Mahidharia <mahidharia.r@northeastern.edu>
2023-12-07 19:56:41 -0500

    configurations for the backend server

--- Final Project/server/index.js
+++ Final Project/server/index.js
@@ -8,3 +8,20 @@ import helmet from "helmet";
 import morgan from "morgan";
 import path from "path";
 import { fileURLToPath } from "url";
+import authRoutes from "./routes/auth.js";
+import userRoutes from "./routes/user.js"
+import { register } from "./controllers/auth.js";
+
+// CONFIGURATION
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+dotenv.config();
+const app = express();
+app.use(express.json);
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin"}));
+app.use(morgan("common"));
+app.use(bodyParser.json({limit: "30mb", extended: true}));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true}));
+app.use(cors());
+app.use("/assests", express.static(path.join(__dirname, 'public/assets')));
