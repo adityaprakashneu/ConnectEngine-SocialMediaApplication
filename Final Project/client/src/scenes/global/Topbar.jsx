328527e46348280b7a98fc57bfa5b0ef53498a3a
Aditya Prakash <prakash.adi@northeastern.edu>
2023-12-07 19:10:19 -0500

    top bar functionality completed

--- Final Project/client/src/scenes/global/Topbar.jsx
+++ Final Project/client/src/scenes/global/Topbar.jsx
@@ -18,3 +18,68 @@ const Topbar = () => {
   const dispatch = useDispatch();
 
   const neutralLight = theme.palette.neutral.light;
+
+  return (
+    <Box display="flex" justifyContent="space-between" p={2}>
+      {/* SEARCH BAR */}
+      <Box
+        display="flex"
+        backgroundColor={colors.primary[400]}
+        borderRadius="3px"
+      >
+        <InputBase sx={{ ml: 2, flex: 1 }} placeholder="Search" />
+        <IconButton type="button" sx={{ p: 1 }}>
+          <SearchIcon />
+        </IconButton>
+      </Box>
+
+      {/* ICONS */}
+      <Box display="flex">
+        <IconButton onClick={colorMode.toggleColorMode}>
+          {theme.palette.mode === "dark" ? (
+            <DarkModeOutlinedIcon />
+          ) : (
+            <LightModeOutlinedIcon />
+          )}
+        </IconButton>
+        <IconButton>
+          <NotificationsOutlinedIcon />
+        </IconButton>
+        <IconButton>
+          <SettingsOutlinedIcon />
+        </IconButton>
+        <IconButton>
+          <PersonOutlinedIcon />
+        </IconButton>
+        <FormControl variant="standard" value="ADMIN">
+              <Select
+                value="ADMIN"
+                sx={{
+                  backgroundColor: neutralLight,
+                  width: "100px",
+                  borderRadius: "0.25rem",
+                  p: "0.25rem 1rem",
+                  "& .MuiSvgIcon-root": {
+                    pr: "0.25rem",
+                    width: "2.75rem",
+                  },
+                  "& .MuiSelect-select:focus": {
+                    backgroundColor: neutralLight,
+                  },
+                }}
+                input={<InputBase />}
+              >
+                <MenuItem value="ADMIN">
+                  <Typography>ADMIN</Typography>
+                </MenuItem>
+                <MenuItem onClick={() => dispatch(setLogout())}>
+                  Log Out
+                </MenuItem>
+              </Select>
+            </FormControl>
+      </Box>
+    </Box>
+  );
+};
+
+export default Topbar;
