a25af4461de85ffc8c7f828b7e566609b07155c1
Aditya Prakash <prakash.adi@northeastern.edu>
2023-12-07 23:20:19 -0500

    invoices functionality page completed

--- Final Project/client/src/scenes/invoices/index.jsx
+++ Final Project/client/src/scenes/invoices/index.jsx
@@ -41,3 +41,43 @@ const Invoices = () => {
       flex: 1,
     },
   ];
+
+  return (
+    <Box m="20px">
+      <Header title="INVOICES" subtitle="List of Invoice Balances" />
+      <Box
+        m="40px 0 0 0"
+        height="75vh"
+        sx={{
+          "& .MuiDataGrid-root": {
+            border: "none",
+          },
+          "& .MuiDataGrid-cell": {
+            borderBottom: "none",
+          },
+          "& .name-column--cell": {
+            color: colors.greenAccent[300],
+          },
+          "& .MuiDataGrid-columnHeaders": {
+            backgroundColor: colors.blueAccent[700],
+            borderBottom: "none",
+          },
+          "& .MuiDataGrid-virtualScroller": {
+            backgroundColor: colors.primary[400],
+          },
+          "& .MuiDataGrid-footerContainer": {
+            borderTop: "none",
+            backgroundColor: colors.blueAccent[700],
+          },
+          "& .MuiCheckbox-root": {
+            color: `${colors.greenAccent[200]} !important`,
+          },
+        }}
+      >
+        <DataGrid checkboxSelection rows={mockDataInvoices} columns={columns} />
+      </Box>
+    </Box>
+  );
+};
+
+export default Invoices;
